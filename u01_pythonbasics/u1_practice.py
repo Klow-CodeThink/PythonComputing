4254da184159607110e2567782b5ac2272ae163c
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-17 09:27:54 +0000

    2025-04-17

---
+++ u01_pythonbasics/u1_practice.py
@@ -0,0 +1,71 @@
+# U1 - Exercise 1
+# Write a program to calculate the perimeter of a rectangle.
+# Example: Assign 12 to length and 8 to width.
+# Calculate the perimeter using the formula: 2 * (length + width).
+
+"""
+length = 12
+breadth = 8
+perimeter = 2 * (length + breadth)
+print("The perimeter of the rectangle is", perimeter,".")
+"""
+
+# U1 - Exercise 2
+# Use variables to create a descriptive sentence.
+# Example: Assign "Alice" to name and "reading" to hobby.
+# Combine them to display: "Alice loves reading in her free time."
+# You must have 2 variables to complete this exercise
+
+"""
+name = "Alice"
+hobby = "reading"
+sentence = name + " loves " + hobby + " in her free time."
+print(sentence)
+"""
+
+# U1 - Exercise 3
+# Combine repetition and a meaningful statement.
+# Example: Print "Let's go! " three times, followed by "We can do it!"
+# Example Output: Let's go! Let's go! Let's go! We can do it!
+# You have to use the string * x method to duplicate the words to complete this exercise.
+
+"""
+cheer = "Let's go! "
+print(cheer * 3 + "We can do it!")
+"""
+
+# U1 - Exercise 4
+# Write a program to manually display a triangle of stars using the print statement.
+# You must use the "string" * x, where x is a number to multiply your strings.
+# Example Output:
+# *
+# **
+# ***
+# ****
+# *****
+
+"""
+print("*")
+print("*" * 2)
+print("*" * 3)
+print("*" * 4)
+print("*" * 5)
+"""
+
+# U1 - Exercise 5
+# Write a program to display a centered triangle of stars with 5 rows
+# # Example Output:
+#     *    
+#    ***  
+#   *****
+#  *******
+# *********
+# Hint: Use spaces and stars with string concatenation (+).
+
+"""
+print("    " + "*")
+print("   " + "*" * 3)
+print("  " + "*" * 5)
+print(" " + "*" * 7)
+print("*" * 9)
+"""
