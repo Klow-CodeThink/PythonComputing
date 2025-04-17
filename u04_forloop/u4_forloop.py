1f0915ccfc54661a857b01fae2da594f8bb8fb76
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-17 04:36:12 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ u04_forloop/u4_forloop.py
@@ -0,0 +1,17 @@
+# for i in range (4):
+#     print("Cheers!")
+#     print(i)
+
+# # print the numbers from 0 to 23
+
+# for i in range(24):
+#     print(i)
+
+name = "TEDDY"
+
+#indentation - space in front of code. 4 spaces 1 tab space
+for c in name:
+    print(f"Give me a {c}")
+
+print("Who is the best?")
+print(name + "!")
