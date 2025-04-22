c73cd6351f9fcf92a9c0dc889f99ec3a1f7efa10
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-22 07:25:22 +0000

    2025-04-22

--- u06_whileloops/u6_practice_exercises.py
+++ u06_whileloops/u6_practice_exercises.py
@@ -4,37 +4,66 @@
 # Write a program to print the multiplication table of 7 up to 10.
 # Example: 7 x 1 = 7, ..., 7 x 10 = 70.
 
-
-
-
+"""
+i = 1
+num = 7
+while if i <= 10:
+        print(f"{num} x {i} = {num * i}")
+        i = i + 1
+"""
 
 #------------------------------------------------------------
 # Exercise 8: Sum of Even Numbers
 # Write a program to calculate the sum of even numbers between 1 
 # and 20 using a while loop. Example: Output = 110.
 
+"""
+i = 2
+sum = 0
+while i <= 20:
+        sum = sum + i
+        i = i + 2
 
-
-
+print(f"The sum is {sum}.")
+"""
 
 #------------------------------------------------------------
 # Exercise 9: Guessing Game
 # Write a program where the user has to guess a random number 
 # between 1 and 10. Keep prompting until they guess correctly.
 
+"""
+import random
+rannum = random.randint(1,10)
+print(rannum)
 
 
+while True:
 
+    guess = int(input("Guess my number: "))
+
+    if guess == rannum:
+        print("You are correct!")
+        break
+    else:
+        print("Try again.")
+"""
 
 #------------------------------------------------------------
 # Exercise 10: Input Validation for a Password
 # Write a program that keeps asking the user to enter a password.
 # If the password is correct, print "Access granted."
 
+"""
+while True:
+    password = input("Enter the password: ")
 
-
-
-
+    if password == "learn2code":
+        print("Access granted.")
+        break
+    else:
+        print("Access denied.")
+"""
 
 #------------------------------------------------------------
 # Exercise 11: Printing Fibonacci Sequence
