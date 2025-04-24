cba27373e99c5c36118bc02384e43e8c29bcccc0
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-24 07:23:40 +0000

    2025-04-24

--- u16_algos/easy1_minmaxsumavg_q2_prac2.py
+++ u16_algos/easy1_minmaxsumavg_q2_prac2.py
@@ -10,3 +10,20 @@
 swim_times = [32.5, 30.1, 33.8, 29.6, 31.2, 34.0, 28.9, 
               30.4, 38.1, 27.5, 35.6, 31.8, 29.2, 33.0, 30.5]
 
+
+fast_time = swim_times[0]
+slow_time = swim_times[0]
+fast_lane = 0
+slow_lane = 0
+
+for i in range(len(swim_times)):
+    if swim_times[i] < fast_time:
+        fast_time = swim_times[i]
+        fast_lane = i + 1
+    
+    if swim_times[i] > slow_time:
+        slow_time = swim_times[i]
+        slow_lane = i + 1
+
+print(f"Lane {fast_lane} has the fastest swim time of {fast_time} seconds.")
+print(f"Lane {slow_lane} has the slowest swim time of {slow_time} seconds.")
--- u16_algos/easy1_minmaxsumavg_q4_prac2.py
+++ u16_algos/easy1_minmaxsumavg_q4_prac2.py
@@ -14,3 +14,39 @@
 daily_sales = [120, 98, 135, 105, 150, 112, 80, 130, 95, 110, 
                102, 85, 140, 99, 123, 145, 78, 90, 136, 145, 
                132, 108, 75, 88, 142, 115, 97, 121, 89, 100]
+
+
+# Find the days with the highest and lowest sales. 
+# Assume that the first item in the list corresponds to Day 1.
+
+high_sales = daily_sales[0]
+low_sales = daily_sales[0]
+h_sales_day = 0
+l_sales_day = 0
+
+for i in range(len(daily_sales)):
+    if daily_sales[i] > high_sales:
+        high_sales = daily_sales[i]
+        h_sales_day = i + 1
+    if daily_sales[i] < low_sales:
+        low_sales = daily_sales[i]
+        l_sales_day = i + 1
+
+print(f"Day {h_sales_day} has the highest sales and Day {l_sales_day} has the lowest sales.")
+
+
+# Find the total sales figure for the month.
+
+thesum = sum(daily_sales)
+
+print(f"The total sales figure for the month is ${thesum}.")
+
+
+# Find the average sales rounded to 2 decimal points.
+
+avg_sales = sum(daily_sales) / len(daily_sales)
+formatted_avg = f"{avg_sales:.2f}"
+
+print(f"The average sales for the month of June is ${formatted_avg}.")
+
+
