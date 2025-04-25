540d415ff23431c874ad936523921bbaf18767f0
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-25 02:30:01 +0000

    2025-04-25

---
+++ u16_algos/easy1_minmaxsumavg_q4_test.py
@@ -0,0 +1,20 @@
+#################################################################
+# Question 4: 
+# A bookstore keeps track of their sales figures each day 
+# for the month of June. The sales for the 30 days are provided 
+# in the list below. 
+# 
+# Find the days with the highest and lowest sales. 
+# Assume that the first item in the list corresponds to Day 1.
+
+# Find the total sales figure for the month.
+# 
+# Find the average sales rounded to 2 decimal points.
+
+daily_sales = [120, 98, 135, 105, 150, 112, 80, 130, 95, 110, 
+               102, 85, 140, 99, 123, 145, 78, 90, 136, 145, 
+               132, 108, 75, 88, 142, 115, 97, 121, 89, 100]
+
+
+# Find the days with the highest and lowest sales. 
+# Assume that the first item in the list corresponds to Day 1.
