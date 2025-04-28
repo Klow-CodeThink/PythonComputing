436e3f1f6ae0da53bdef8a8ebc20a27a323e86d1
Klow-CodeThink <keiji.low@computhink.com.sg>
2025-04-28 18:53:36 +0800

    2025-04-28

--- u14_functions/U14_challenge_exercises.py
+++ u14_functions/U14_challenge_exercises.py
@@ -1,4 +1,79 @@
 ###########################################################
 # Part 3: Challenge Exercises
 
-# Exercise 1: 
+# Exercise 1: Random Username Generator
+# Write a function that generates a username based on the user's
+# first name, last name, a random animal from a list, and a random number.
+
+# List of random animals: ["Tiger", "Lion", "Bear", "Wolf", "Eagle"]
+# Random number: Generated between 10 and 99
+
+# Example:
+# Input: First Name: "John", Last Name: "Doe"
+# Random animal: "Tiger", Random number: 42
+# Output: "JohnDoeTiger42"
+
+
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 2: 
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 3: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 4: 
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------
+# Exercise 5: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################################
